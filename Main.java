cbcd4117e8f6737365dcd3ba1a736ceda08875d9
wrathsc <127319280+wrathsc@users.noreply.github.com>
2023-04-05 12:26:09 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[]args) {
+  int wysokosc = 4;
+  int szerokosc=6;
+  for(int i=0; i<wysokosc; i++) {
+    for(int j=0; j<szerokosc; j++) {
+      System.out.print("*");
+    }
+  
+    System.out.println();
+  }
+}
+}
